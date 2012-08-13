415071243194bf4681200e25c05b49c18d22dd02
shen.shen <shen.shen@outlook.com>
2012-08-13 22:10:27 +0100

    first commit

---
+++ Test/src/myPackage/MyFirstClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myPackage;
+
+/**
+ * @author ss
+ *
+ */
+public class MyFirstClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
